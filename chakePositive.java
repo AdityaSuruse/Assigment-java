557cc06280a2582200e82c34157cd5949a585e88
aditya <adityasurusee9@gmail.com>
2025-08-23 15:14:06 +0530

    commit

--- chakePositive.java
+++ chakePositive.java
@@ -1,14 +1,15 @@
 import java.util.Scanner;
-class chakePositive{
+
+class chakePositive {
     public static void main(String[] args) {
         int a;
-        Scanner sc=new Scanner(System.in);
+        Scanner sc = new Scanner(System.in);
         System.out.println("Enter A Number :");
-        a=sc.nextInt();
- if (a>0) {
-     System.out.println("Positive Number : " +a);
- } else {
-     System.out.println("Negative Number : " +a);
- }
+        a = sc.nextInt();
+        if (a > 0) {
+            System.out.println("Positive Number : " + a);
+        } else {
+            System.out.println("Negative Number : " + a);
+        }
     }
 }
